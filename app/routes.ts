59ff27d71d2a849e62e2d7d973877a6ebd7e92d2
gelik <136448734+mysitessa@users.noreply.github.com>
2025-07-29 14:27:27 +0300

    file for path and routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,11 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
-
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+import {
+    type RouteConfig,
+    route,
+  } from "@react-router/dev/routes";
+  
+  export default [
+    route("/", "routes/home.tsx"),
+    route("/hello", "routes/hello.tsx"),
+    route('/info', 'routes/info.tsx'),
+  ] satisfies RouteConfig;
+  
