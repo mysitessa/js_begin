d294e1c6bdfe43e2aa2b8e9fcf4ac27df13bba05
gelik <136448734+mysitessa@users.noreply.github.com>
2025-07-29 14:25:42 +0300

    внешний вид страницы hello

---
+++ app/routes/hello.tsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <div className="hello-page">
+        <h1>Hello World!</h1>
+        <p>This is a class component route</p>
+      </div>
+    );
+  }
+}
